a5e0985f79ad5409d2023260abcdbf9bfd08a056
jhonace-jaf <jhonacesasutona718@gmail.com>
2026-04-08 17:36:11 +0800

    feat: hide footer in MainLayout component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <MainLayout>
+      <MainLayout showFooter={false}>
         <Home />
       </MainLayout>
     </ThemeProvider>
